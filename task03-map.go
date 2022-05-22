7739ba9b33e8311cac409cab39d0d58af12a49d9
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-05-22 20:37:09 +0400

    task3

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,22 @@
 package homework
 
-func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+import (
+	"sort"
+)
+
+
+func SortMapValues(input map[int]string) (result []string) {
+	var values []string
+	var keys []int
+	
+	for k := range input {
+		keys = append(keys, k)
+		sort.Ints(keys[:])
+	}
+	
+	for _, v := range keys {
+		values = append(values, input[v])
+	}
+
+	return values
 }
