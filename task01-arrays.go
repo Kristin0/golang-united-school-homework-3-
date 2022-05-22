d94eb188763b449a18a0a0d83e367d71a93efc94
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-05-22 17:29:41 +0400

    task1

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,11 +1,17 @@
 package homework
 
-func average(input []float32) (result float32) {
 
+
+func Average(input [15]float32) (result float32) {
+
+    var count int
 	var sum float32 = 0
 	var num int = len(input)
 	for i := 0; i < num; i++ {
-		sum += input[i]
+		if input[i] != 0 {
+			sum += input[i]
+			count++
+		}
 	}
-	return sum/float32(num)
+	return sum/float32(count)
 }
